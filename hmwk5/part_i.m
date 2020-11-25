7e22e2e6852e951f9d76fd5a350ef7464ee490fa
Mojan Hamed <mojan.hamed@gmail.com>
2020-11-25 00:26:20 -0500

    Update part_i.m

--- hmwk5/part_i.m
+++ hmwk5/part_i.m
@@ -13,3 +13,20 @@ A_tril = A_triu.' - diag(diag(A_triu));
 A = A_tril + A_triu;
 
 [eigenvec, eigenval] = eigs(A,m);
+
+% b) 
+true_eigenvalue = max(diag(eigenval));
+v_curr = ones(m,1);
+
+% power iteration method
+for k = 1:20
+	w = A*v_curr;
+	v_curr=w/norm(w);
+
+	lambda(k) = v_curr'*A*v_curr;
+    power_eigenvalue = max(lambda);
+    error(k) = abs(true_eigenvalue - power_eigenvalue);
+end
+
+plot(error); xlim([1 20]); ylabel('error'); xlabel('iterations'); 
+title('Error of Power Iteration Max Eigenvalue by Iteration');
